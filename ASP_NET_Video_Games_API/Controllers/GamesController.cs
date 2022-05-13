c6c2d32d40bc220f3161448d12b27e1aacef15ef
Dye <anthony.dye@ies-co.com>
2022-05-13 10:26:37 -0500

    Created Both Get Requests in the Controllers File

---
+++ ASP_NET_Video_Games_API/Controllers/GamesController.cs
@@ -0,0 +1,36 @@
+﻿using ASP_NET_Video_Games_API.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace ASP_NET_Video_Games_API.Controllers
+{
+    //api/games
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GamesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GamesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetVideoGames()
+        {
+            var getVideoGames = _context.VideoGames.ToList();
+
+            return Ok(getVideoGames);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetGamesById(int id)
+        {
+            var videoGames = _context.VideoGames.Where(vg => vg.Id == id);
+
+            return Ok(videoGames);
+        }
+    }
+}
